ded469a7398a97c1baf2c1af291aa1dff705ad6d
Serena <serenalambert1731@gmail.com>
2020-01-28 19:20:44 +0000

    header and footer created for recipe page

--- AvocadoRecipes/View/ContentView.swift
+++ AvocadoRecipes/View/ContentView.swift
@@ -10,7 +10,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Recipes")
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                
+                // MARK: - HEADER
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(alignment: .top, spacing: 0) {
+                        HeaderView()
+                    }
+                }
+                    
+                
+                
+                // MARK: - FOOTER
+                
+                VStack(alignment: .center, spacing: 20) {
+                    Text("All about avocados")
+                        .font(.system(.title, design: .serif))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("ColorGreenAdaptive"))
+                        .padding(8)
+                    Text("Everything you wanted to know about avocados but were too afraid to ask.")
+                        .font(.system(.body, design: .serif))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.gray)
+                }
+                .frame(maxWidth: 640)
+                .padding()
+                .padding(.bottom, 85)
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
